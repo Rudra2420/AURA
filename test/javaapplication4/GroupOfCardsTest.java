4a19ac335a9a12c1da14c5d7c9dcc827d216ebca
rudra <rudra@192.168.2.27>
2022-11-29 09:08:43 -0500

    CMD TRY

---
+++ test/javaapplication4/GroupOfCardsTest.java
@@ -0,0 +1,133 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package javaapplication4;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 14168
+ */
+public class GroupOfCardsTest {
+    
+    public GroupOfCardsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of showCards method, of class GroupOfCards.
+     */
+//    @Test
+//    public void testShowCards() {
+//        System.out.println("showCards");
+//        GroupOfCards instance = null;
+//        ArrayList<playingCard> expResult = null;
+//        ArrayList<playingCard> result = instance.showCards();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of shuffle method, of class GroupOfCards.
+//     */
+//    @Test
+//    public void testShuffle() {
+//        System.out.println("shuffle");
+//        GroupOfCards instance = null;
+//        instance.shuffle();
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getSize method, of class GroupOfCards.
+//     */
+//    @Test
+//    public void testGetSize() {
+//        System.out.println("getSize");
+//        GroupOfCards instance = null;
+//        int expResult = 0;
+//        int result = instance.getSize();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of setSize method, of class GroupOfCards.
+//     */
+    @Test
+    public void testSetSize() {
+        System.out.println("setSize");
+        int givenSize = 22;
+        GroupOfCards instance = null;
+        instance.setSize(givenSize);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+//
+//    /**
+//     * Test of drawCard method, of class GroupOfCards.
+//     */
+//    @Test
+//    public void testDrawCard() {
+//        System.out.println("drawCard");
+//        GroupOfCards instance = null;
+//        playingCard expResult = null;
+//        playingCard result = instance.drawCard();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getCards method, of class GroupOfCards.
+//     */
+//    @Test
+//    public void testGetCards() {
+//        System.out.println("getCards");
+//        GroupOfCards instance = null;
+//        ArrayList<playingCard> expResult = null;
+//        ArrayList<playingCard> result = instance.getCards();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of setCards method, of class GroupOfCards.
+//     */
+//    @Test
+//    public void testSetCards() {
+//        System.out.println("setCards");
+//        ArrayList<playingCard> cards = null;
+//        GroupOfCards instance = null;
+//        instance.setCards(cards);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    
+}
